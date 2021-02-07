84532edcf09d1b0b063b50d60ed6f0c19119a6fa
coera <coera@pr11.fr>
2021-02-07 19:03:22 +0000

    modifications

--- main.py
+++ main.py
@@ -1,116 +1,58 @@
 from PIL import Image
 from PIL import ImageFilter
+import wget
+import os
 
+url = -1
 analyse = -1
-analyse = input("Rentre le chemin l'image à analyser: ")
+analyse = url = input("Rentre le chemin l'image à analyser: ")
 
-if analyse == "img/lenny.jpg" :
+if url != "text" :
+  wget.download(url, 'img')
+  
+  img_name = -1
+  img_name = input(" Niquel ! Quel était le nom de l'image ? ") 
 
-  lenny_type = -1
-  lenny_type = input("Niquel ! Tu as choisi lenny.jpg. Dis moi ce que tu veux faire: ")
+  if img_name != "text" :
 
-  if lenny_type == "find-eges" :
-    image = Image.open("img/lenny.jpg") 
-    image = image.filter(ImageFilter.FIND_EDGES)
-    image = image.save("end/lenny.jpg")
-    print("C'est bon")
-  if lenny_type == "contour" :
-    image = Image.open("img/lenny.jpg") 
-    image = image.filter(ImageFilter.CONTOUR)
-    image = image.save("end/lenny.jpg")
-    print("C'est bon")
-  if lenny_type == "blur" :
-    image = Image.open("img/lenny.jpg") 
-    image = image.filter(ImageFilter.BLUR)
-    image = image.save("end/lenny.jpg")
-    print("C'est bon")
-  if lenny_type == "maxi-blur" :
-    image = Image.open("img/lenny.jpg") 
-    image = image.filter(ImageFilter.GaussianBlur(20))
-    image = image.save("end/lenny.jpg")
-    print("C'est bon")
-  if lenny_type == "enhance-max" :
-    image = Image.open("img/lenny.jpg") 
-    image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
-    image = image.save("end/lenny.jpg")
-    print("C'est bon")
-  if lenny_type == "enhance" :
-    image = Image.open("img/lenny.jpg")      
-    image = image.filter(ImageFilter.EDGE_ENHANCE)
-    image = image.save("end/lenny.jpg")
-    print("C'est bon")
+    img_type = -1
+    img_type = input("Parfait ! Dit moi ce que je dois faire avec " + img_name + ": ")
 
+    if img_type == "find-eges" :
+      image = Image.open("img/" + img_name) 
+      image = image.filter(ImageFilter.FIND_EDGES)
+      image = image.save("end/" + img_name)
+      os.remove("img/" + img_name)
+      print("C'est bon ! Ton image est stockée dans end/" + img_name)
+    if img_type == "contour" :
+      image = Image.open("img/" + img_name) 
+      image = image.filter(ImageFilter.CONTOUR)
+      image = image.save("end/" + img_name)
+      os.remove("img/" + img_name)
+      print("C'est bon ! Ton image est stockée dans end/" + img_name)
+    if img_type == "blur" :
+      image = Image.open("img/" + img_name) 
+      image = image.filter(ImageFilter.BLUR)
+      image = image.save("end/" + img_name)
+      os.remove("img/" + img_name)
+      print("C'est bon ! Ton image est stockée dans end/" + img_name)
+    if img_type == "maxi-blur" :
+      image = Image.open("img/" + img_name) 
+      image = image.filter(ImageFilter.GaussianBlur(20))
+      image = image.save("end/" + img_name)
+      os.remove("img/" + img_name)
+      print("C'est bon ! Ton image est stockée dans end/" + img_name)
+    if img_type == "enhance-max" :
+      image = Image.open("img/" + img_name) 
+      image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+      image = image.save("end/" + img_name)
+      os.remove("img/" + img_name)
+      print("C'est bon ! Ton image est stockée dans end/" + img_name)
+    if img_type == "enhance" :
+      image = Image.open("img/" + img_name)      
+      image = image.filter(ImageFilter.EDGE_ENHANCE)
+      image = image.save("end/" + img_name)
+      os.remove("img/" + img_name)
+      print("C'est bon ! Ton image est stockée dans end/" + img_name)
 
-if analyse == "img/cat.png" :
-
-  cat_type = -1
-  cat_type = input("Niquel ! Tu as choisi cat.png. Dis moi ce que tu veux faire: ")
-
-  if cat_type == "find-edges" :
-    image = Image.open("img/cat.png") 
-    image = image.filter(ImageFilter.FIND_EDGES)
-    image = image.save("end/cat.png")
-    print("C'est bon")
-  if cat_type == "contour" :
-    image = Image.open("img/cat.png") 
-    image = image.filter(ImageFilter.CONTOUR)
-    image = image.save("end/cat.png")
-    print("C'est bon")
-  if cat_type == "blur" :
-    image = Image.open("img/cat.png") 
-    image = image.filter(ImageFilter.BLUR)
-    image = image.save("end/cat.png")
-    print("C'est bon")
-  if cat_type == "maxi-blur" :
-    image = Image.open("img/cat.png") 
-    image = image.filter(ImageFilter.GaussianBlur(20))
-    image = image.save("end/cat.png")
-    print("C'est bon")
-  if cat_type == "enhance-max" :
-    image = Image.open("img/cat.png") 
-    image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
-    image = image.save("end/cat.png")
-    print("C'est bon")
-  if cat_type == "enhance" :
-    image = Image.open("img/cat.png") 
-    image = image.filter(ImageFilter.EDGE_ENHANCE)
-    image = image.save("end/cat.png")
-    print("C'est bon")
-
-
-if analyse == "img/logo.png" :
-
-  logo_type = -1
-  logo_type = input("Niquel Tu as choisi logo.png. Dis moi ce que tu veux faire: ")
-
-  if logo_type == "find-edges" :
-    image = Image.open("img/logo.png") 
-    image = image.filter(ImageFilter.FIND_EDGES)
-    image = image.save("end/logo.png")
-    print("C'est bon")
-  if logo_type == "contour" :
-    image = Image.open("img/logo.png") 
-    image = image.filter(ImageFilter.CONTOUR)
-    image = image.save("end/logo.png")
-    print("C'est bon")
-  if logo_type == "blur" :
-    image = Image.open("img/logo.png") 
-    image = image.filter(ImageFilter.BLUR)
-    image = image.save("end/logo.png")
-    print("C'est bon")
-  if logo_type == "maxi-blur" :
-    image = Image.open("img/logo.png") 
-    image = image.filter(ImageFilter.GaussianBlur(20))
-    image = image.save("end/logo.png")
-    print("C'est bon")
-  if logo_type == "enhance-max" :
-    image = Image.open("img/logo.png") 
-    image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
-    image = image.save("end/logo.png")
-    print("C'est bon")
-  if logo_type == "enhance" :
-    image = Image.open("img/logo.png") 
-    image = image.filter(ImageFilter.EDGE_ENHANCE)
-    image = image.save("end/logo.png")
-    print("C'est bon")
 
